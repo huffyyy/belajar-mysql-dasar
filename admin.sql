6f528aa996f1801c6bdd9b80e7519f65d804acf3
Husnul <husnulf936@gmail.com>
2025-12-01 13:43:20 +0700

    Auto Increment

---
+++ admin.sql
@@ -0,0 +1,24 @@
+CREATE TABLE admin
+(
+    id         INT          NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name  VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+DESCRIBE admin;
+
+INSERT INTO admin (first_name, last_name)
+VALUES ('Husnul', 'Fikri');
+INSERT INTO admin (first_name, last_name)
+VALUES ('Rasasi', 'Hawas');
+INSERT INTO admin (first_name, last_name)
+VALUES ('Adidas', 'Spezial');
+
+DELETE
+FROM admin
+WHERE id = 4;
+
+SELECT *
+FROM admin;
+
